aaf3a8515d243695dcaa6358ba4674612c0cf1c4
Nick Lobanov <nk.nicolas142@gmail.com>
2020-05-08 14:52:06 +0300

    create section stories

---
+++ components/SectionSubtitle.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="section-subtitle">
+    <p class="section-subtitle__text">Истории неизлечимых привычек</p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.section-subtitle {
+  width: 413px;
+  margin-bottom: 70px;
+}
+
+.section-subtitle__text {
+  font-weight: 600;
+  font-size: 32px;
+  line-height: 36px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,37 @@
 <template>
   <div class="container">
     <main-title />
+
+    <section class="stories">
+      <stories-rectangle></stories-rectangle>
+      <section-subtitle />
+      <div class="stories__wrap">
+        <stories-item />
+        <stories-item />
+        <stories-item />
+        <stories-item />
+        <stories-item />
+        <stories-item />
+        <stories-item />
+        <stories-item />
+      </div>
+      <button class="stories__button">Больше статей</button>
+    </section>
   </div>
 </template>
 
 <script>
 import MainTitle from '@/components/MainTitle';
+import Rectangle from '@/components/Rectangle';
+import SectionSubtitle from '@/components/SectionSubtitle';
+import StoriesItem from '@/components/StoriesItem';
 
 export default {
   components: {
     'main-title': MainTitle,
+    'stories-rectangle': Rectangle,
+    'section-subtitle': SectionSubtitle,
+    'stories-item': StoriesItem,
   },
 };
 </script>
@@ -20,5 +42,26 @@ export default {
   flex-direction: column;
   justify-content: flex-start;
   align-items: center;
+}
+
+.stories {
+  width: 92%;
+}
+
+.stories__wrap {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+
+.stories__button {
+  display: block;
+  border: none;
+  background-color: transparent;
+  width: 100%;
+  text-align: center;
+  font-weight: normal;
+  font-size: 16px;
+  line-height: 20px;
 }
 </style>
