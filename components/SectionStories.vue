eafd9cc0703e041e70f1d6a35e7d5337ff721b87
Nick Lobanov <nk.nicolas142@gmail.com>
2020-05-09 16:44:23 +0300

    fix section stories

---
+++ components/SectionStories.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="stories">
+    <rectangle></rectangle>
+    <section-subtitle></section-subtitle>
+    <slot></slot>
+  </section>
+</template>
+
+<script>
+import Rectangle from '@/components/Rectangle';
+import SectionSubtitle from '@/components/SectionSubtitle';
+
+export default {
+  components: {
+    rectangle: Rectangle,
+    'section-subtitle': SectionSubtitle,
+  },
+};
+</script>
+
+<style scoped>
+.stories {
+  width: 92%;
+}
+</style>
---
+++ components/StoriesWrap.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="stories__wrap">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.stories__wrap {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+</style>
