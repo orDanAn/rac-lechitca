a7d95da5649dd4d92d99dea7dd90568d7237bbfa
Alexander Maximkin <maximkin.alek@yandex.ru>
2020-05-09 15:06:28 +0300

    Создал footer и добавил его в index

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,20 @@
 <template>
-  <div class="container">
-    <main-title />
-  </div>
+  <body>
+    <div class="container">
+      <main-title />
+    </div>
+    <project-footer />
+  </body>
 </template>
 
 <script>
 import MainTitle from '@/components/MainTitle';
+import Footer from '@/components/Footer';
 
 export default {
   components: {
     'main-title': MainTitle,
+    'project-footer': Footer,
   },
 };
 </script>
