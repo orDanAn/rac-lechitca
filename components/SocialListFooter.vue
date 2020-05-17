8372bf80b36eccf2524fd338079a645d9de7e0fc
Alexander Maximkin <maximkin.alek@yandex.ru>
2020-05-17 17:43:36 +0300

    fixed the styles and name of the social footer

--- components/SocialListFooter.vue
+++ components/SocialListFooter.vue
@@ -1,19 +1,21 @@
 <template>
-  <nav class="footer__menu">
-    <ul class="footer__list">
-      <li class="footer__list-elem">
-        <a href="https://www.instagram.com/raklechitsa/" class="footer__link"
+  <nav class="social-list">
+    <ul class="social-list__list">
+      <li class="social-list__list-elem">
+        <a
+          href="https://www.instagram.com/raklechitsa/"
+          class="social-list__link"
           >Мы в Инстаграме</a
         >
-        <span class="footer__link">и</span>
+        <span class="social-list__link">и</span>
         <a
           href="https://www.youtube.com/results?search_query=%23%D1%8D%D1%82%D0%BE%D0%BD%D0%B5%D0%BB%D0%B5%D1%87%D0%B8%D1%82%D1%81%D1%8F"
-          class="footer__link"
+          class="social-list__link"
           >Youtube</a
         >
       </li>
-      <li class="footer__list-elem">
-        <button-open-popup class="footer__button"
+      <li class="social-list__list-elem">
+        <button-open-popup class="social-list__button"
           >Поделитесь ↗</button-open-popup
         >
       </li>
@@ -31,7 +33,7 @@ export default {
 </script>
 
 <style scoped>
-.footer__list {
+.social-list__list {
   display: flex;
   flex-direction: column;
   justify-content: space-between;
@@ -39,7 +41,7 @@ export default {
   min-height: 94px;
   margin-right: 42px;
 }
-.footer__link {
+.social-list__link {
   font-style: normal;
   font-weight: normal;
   font-size: 18px;
@@ -48,21 +50,21 @@ export default {
   color: black;
   transition: opacity 0.3s ease-in-out;
 }
-.footer__link:hover {
+.social-list__link:hover {
   opacity: 0.8;
 }
-.footer__button {
+.social-list__button {
   font-size: 18px;
   line-height: 24px;
 }
 @media screen and (max-width: 1280px) {
-  .footer__link {
+  .social-list__link {
     font-size: 16px;
   }
-  .footer__list {
+  .social-list__list {
     min-height: 90px;
   }
-  .footer__button {
+  .social-list__button {
     font-size: 16px;
   }
 }
