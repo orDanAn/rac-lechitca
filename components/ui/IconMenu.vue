4e5af196d4430fec0ca71912e0c792e046c2d386
Alexander Maximkin <maximkin.alek@yandex.ru>
2020-05-19 21:43:18 +0300

    adaptiv for every breakpoints

---
+++ components/ui/IconMenu.vue
@@ -0,0 +1,18 @@
+<template>
+  <i class="icon-menu"></i>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.icon-menu {
+  width: 32px;
+  height: 27px;
+  background-image: url('/ui-image/Icon-menu.svg');
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+}
+</style>
