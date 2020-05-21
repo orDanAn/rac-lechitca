63e0acafe2f5773afa71ecb6c7f6175b90ff8a71
Roman-Statsura <srs1012@yandex.ru>
2020-05-21 21:30:47 +0400

    adapted Input

--- components/ui/InputSearchStory.vue
+++ components/ui/InputSearchStory.vue
@@ -14,4 +14,28 @@ export default {};
   width: 1074px;
   min-height: 52px;
 }
+@media (min-width: 1280px) and (max-width: 1439px) {
+  .input-search {
+    width: 934px;
+    min-height: 48px;
+  }
+}
+@media (min-width: 1024px) and (max-width: 1279px) {
+  .input-search {
+    width: 696px;
+    min-height: 48px;
+  }
+}
+@media (min-width: 768px) and (max-width: 1023px) {
+  .input-search {
+    width: 460px;
+    min-height: 46px;
+  }
+}
+@media (min-width: 320px) and (max-width: 767px) {
+  .input-search {
+    width: 238px;
+    min-height: 46px;
+  }
+}
 </style>
