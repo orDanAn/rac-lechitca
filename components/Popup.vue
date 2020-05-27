15ed9b7b92f39876d2a2d04b7c148c21eaa2db5d
Roman-Statsura <srs1012@yandex.ru>
2020-05-27 23:02:58 +0400

    Fixed Bem errors in the section Video and Title_section, ContentContact.Also added scroll in the section ConentContact

--- components/Popup.vue
+++ components/Popup.vue
@@ -18,5 +18,7 @@ export default {};
   width: 100%;
   height: 100%;
   background-color: rgba(0, 0, 0, 0.5);
+  max-height: 100vh;
+  overflow-y: auto;
 }
 </style>
--- components/Title_section.vue
+++ components/Title_section.vue
@@ -1,7 +1,5 @@
 <template>
-  <div class="title">
-    <h3 class="title__text"><slot>ЗАГОЛОВОК</slot></h3>
-  </div>
+  <h3 class="title-text"><slot>ЗАГОЛОВОК</slot></h3>
 </template>
 
 <script>
@@ -9,7 +7,7 @@ export default {};
 </script>
 
 <style scoped>
-.title__text {
+.title-text {
   width: 413px;
   font-style: normal;
   font-weight: 600;
@@ -18,28 +16,28 @@ export default {};
 }
 
 @media (min-width: 1280px) and (max-width: 1439px) {
-  .title__text {
+  .title-text {
     width: 367px;
     font-size: 28px;
     line-height: 32px;
   }
 }
 @media (min-width: 1024px) and (max-width: 1279px) {
-  .title__text {
+  .title-text {
     width: 288px;
     font-size: 24px;
     line-height: 28px;
   }
 }
 @media (min-width: 768px) and (max-width: 1023px) {
-  .title__text {
+  .title-text {
     width: 380px;
     font-size: 24px;
     line-height: 28px;
   }
 }
 @media (min-width: 320px) and (max-width: 767px) {
-  .title__text {
+  .title-text {
     width: 290px;
     font-size: 18px;
     line-height: 21px;
